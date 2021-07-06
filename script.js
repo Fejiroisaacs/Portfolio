a99036c9fe8ae81f3a998be8af73c2ee26c72045
Fejiroisaacs <fejiroisaac@gmail.com>
2021-07-06 09:29:28 -0400

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var time = h + ":" + m + ":" + s;
+    $(".time").text(time);
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    } // add zero in front of numbers < 10
+    return i;
+}
