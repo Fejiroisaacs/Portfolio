5ab506a8f5b3d2da7ee9b6cd65fee8c2d0bb7aa1
Fejiroisaacs <fejiroisaac@gmail.com>
2021-07-09 09:29:08 -0400

    added code to keep menu bar at the top

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
+    h = checkTime(h);
     var time = h + ":" + m + ":" + s;
     $(".time").text(time);
     setTimeout(startTime, 1000);
@@ -13,7 +14,7 @@ function startTime() {
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
-    } // add zero in front of numbers < 10
+    } // adds zero in front of numbers less than 10
     return i;
 }
 $(document).ready(function() {
@@ -23,13 +24,19 @@ $(".change").click(function(){
     var innerColor =$("#inner-color").val() ;
     var textCOlor =$("#text-color").val();
     var fontsize =$("#fontsize").val();
-    theme(outerColor, innerColor, textCOlor, fontsize);
+    var hoverColor=$("#hover-color").val();
+    theme(outerColor, innerColor, textCOlor, fontsize, hoverColor);
 });
 });
 
-function theme(outer, inner, text, font){
+function theme(outer, inner, text, font, hover){
     $(".section").css("background-color", outer);
     $(".innersection").css("background-color", inner);
     $("p,h1").css("color",text);
     $("p").css("font-size", font + "px");
+    $("p").hover(function(){
+        $("p").css("color", hover);
+        }, function(){
+        $("p").css("color", text);
+      });      
 }
