5e5591fba00a73b828314a716791c022f8fdb0d8
Fejiroisaacs <fejiroisaac@gmail.com>
2021-07-13 20:40:15 -0400

    minor update

--- script.js
+++ script.js
@@ -43,3 +43,12 @@ window.addEventListener('scroll', function () {
             let windowPosition = window.scrollY > 0;
             header.classList.toggle('scrolling-active', windowPosition);
         })
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
